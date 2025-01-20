a35a31ae8543308e16d61e53fe500223e7241aca
AsgharKazmi2005 <asgharkazmi05@gmail.com>
2025-01-20 18:57:49 -0500

    Google Calendar Integration
    Added google calendar integrations for the auto scheduler.

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css";
 const App = () => {
   return (
     <div>
-      <h1>NYIT Final Exams Schedule</h1>
+      <h1>NYIT Final Exams Schedule - NYC FALL 2024</h1>
       <Table />
     </div>
   );
